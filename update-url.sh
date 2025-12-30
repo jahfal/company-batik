00913b2a4910b56d6493aac75287056b25c1903f
jahfal <jahfal.lazuardi@sinarmasland.com>
2025-12-30 16:26:10 +0700

    adjustment update-url

--- update-url.sh
+++ update-url.sh
@@ -20,7 +20,8 @@ sed -i "s|$TEMPLATE_URL|$NEW_URL|g" company-profile-batik/Dockerfile
 sed -i "s|$TEMPLATE_URL|$NEW_URL|g" dashboard-cms/Dockerfile
 
 echo "🏗️  Membangun ulang container..."
-docker-compose up -d --build frontend cms
+docker compose build --no-cache frontend cms_app
+docker compose up -d frontend cms_app
 
 # KEMBALIKAN KE TEMPLATE (PENTING!)
 # Agar saat nanti kamu git pull lagi, tidak ada konflik
