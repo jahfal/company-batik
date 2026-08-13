3b726befda8c511c9902c1a4b3db68e2e8fb6735
jahfal <jahfallazuardi@gmail.com>
2026-08-13 16:11:46 +0700

    chore: changes api port

--- company-profile-batik/src/config/api.js
+++ company-profile-batik/src/config/api.js
@@ -1,3 +1,3 @@
 // src/config/api.js
-export const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL || "https://api.jahfallazuardi.my.id/api";
-export const API_URL = process.env.NEXT_PUBLIC_API_URL || "https://api.jahfallazuardi.my.id/api";
+export const API_BASE_URL = "https://api.jahfallazuardi.my.id/api";
+export const API_URL = "https://api.jahfallazuardi.my.id/api";
